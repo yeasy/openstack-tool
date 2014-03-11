a992431f3e4c2758440338c6f491e43afeb970cc
Baohua Yang <baohyang@cn.ibm.com>
2014-03-11 15:52:00 +0800

    Clear the openstack setup from the machine

---
+++ clear_openstack.sh
@@ -0,0 +1,38 @@
+# Warning! Dangerous step! Destroys VMs
+for x in $(virsh list --all | grep instance- | awk '{print $2}') ; do
+    virsh destroy $x ;
+    virsh undefine $x ;
+done ;
+
+# Warning! Dangerous step! Removes lots of packages
+yum remove -y nrpe "*nagios*" puppet "*ntp*" "*openstack*" \
+"*nova*" "*keystone*" "*glance*" "*cinder*" "*swift*" \
+mysql mysql-server httpd "*memcache*" scsi-target-utils \
+iscsi-initiator-utils perl-DBI perl-DBD-MySQL ;
+
+ps -ef | grep -i repli | grep swift | awk '{print $2}' | xargs kill ;
+
+# Warning! Dangerous step! Deletes local application data
+rm -rf /etc/nagios /etc/yum.repos.d/packstack_* /root/.my.cnf \
+/var/lib/mysql/ /var/lib/glance /var/lib/nova /etc/nova /etc/swift \
+/srv/node/device*/* /var/lib/cinder/ /etc/rsync.d/frag* \
+/var/cache/swift /var/log/keystone /var/log/cinder/ /var/log/nova/ \
+/var/log/httpd /var/log/glance/ /var/log/nagios/ /var/log/quantum/ ;
+
+umount /srv/node/device* ;
+killall -9 dnsmasq tgtd httpd ;
+setenforce 1 ;
+vgremove -f cinder-volumes ;
+losetup -a | sed -e 's/:.*//g' | xargs losetup -d ;
+find /etc/pki/tls -name "ssl_ps*" | xargs rm -rf ;
+for x in $(df | grep "/lib/" | sed -e 's/.* //g') ; do
+    umount $x ;
+done
+
+ovs-vsctl --if-exists del-port br-int patch-tun
+ovs-vsctl --if-exists del-port br-tun patch-int
+ovs-vsctl --if-exists del-port br-tun gre-1
+ovs-vsctl --if-exists del-port br-tun gre-2
+ovs-vsctl --if-exists del-br br-tun
+
+yum -y update; yum makecache
