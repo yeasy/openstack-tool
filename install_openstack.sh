7812dc27cc2b6bf6dc6a4b4792496c52683cee36
Baohua Yang <baohyang@cn.ibm.com>
2014-03-14 17:12:13 +0800

    Remove unnecessary-to-install packages

--- install_openstack.sh
+++ install_openstack.sh
@@ -3,6 +3,6 @@
 yum install -y http://rdo.fedorapeople.org/openstack/openstack-havana/rdo-release-havana.rpm  
 rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-yum install -y openstack-packstack firefox nagios nagios-plugins-all nagios-plugins-nrpe nrpe php httpd
+yum install -y openstack-packstack nagios nagios-plugins-all nagios-plugins-nrpe nrpe
 
 packstack --answer-file packstack-answers-vlan.txt
