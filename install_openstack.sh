8f5c9d9cf6d4db9b023795da5c6cf9b9fd36e3af
Baohua Yang <baohyang@cn.ibm.com>
2014-03-16 14:07:47 +0800

    Add some comments

--- install_openstack.sh
+++ install_openstack.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+echo "Recommend to run the ./clear_openstack.sh first, especially for alreay installed machines."
+
 yum install -y http://rdo.fedorapeople.org/openstack/openstack-havana/rdo-release-havana.rpm  
 rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 yum install -y openstack-packstack nagios nagios-plugins-all nagios-plugins-nrpe nrpe
-service ntpd stop
 
+service ntpd stop
 ssh 192.168.122.101 "service ntpd stop"
 
+#for vlan mode
 packstack --answer-file packstack-answers-vlan.txt
+
+#for gre mode
+#packstack --answer-file packstack-answers-gre.txt
