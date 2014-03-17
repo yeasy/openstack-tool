0aa281ca4081bc0781ddca21e9ba59ff1d6dc268
Baohua Yang <baohyang@cn.ibm.com>
2014-03-17 10:24:42 +0800

    Fix the slash problem with sed

--- install_openstack.sh
+++ install_openstack.sh
@@ -8,11 +8,11 @@ rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 yum install -y openstack-packstack nagios nagios-plugins-all nagios-plugins-nrpe nrpe puppet
 
 #Avoid the failure occured by mirrorlist sometimes
-sed -i 's/^mirrorlist=https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch/#mirrorlist=https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch/g' /etc/yum.repos.d/epel.repo
+sed -i 's/^mirrorlist=https:\/\/mirrors.fedoraproject.org\/metalink?repo=epel-6&arch=$basearch/#mirrorlist=https:\/\/mirrors.fedoraproject.org\/metalink?repo=epel-6&arch=$basearch/g' /etc/yum.repos.d/epel.repo
 
 service ntpd stop
 ssh 192.168.122.101 "service ntpd stop"
-ssh 192.168.122.101 "sed -i 's/^mirrorlist=https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch/#mirrorlist=https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch/g' /etc/yum.repos.d/epel.repo"
+ssh 192.168.122.101 "sed -i 's/^mirrorlist=https:\/\/mirrors.fedoraproject.org\/metalink?repo=epel-6&arch=$basearch/#mirrorlist=https:\/\/mirrors.fedoraproject.org\/metalink?repo=epel-6&arch=$basearch/g' /etc/yum.repos.d/epel.repo"
 
 #for vlan mode
 packstack --answer-file packstack-answers-vlan.txt
