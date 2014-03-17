c2249d463d48f74e8a4b80d4ead3abc50cc5dd6a
Baohua Yang <baohyang@cn.ibm.com>
2014-03-17 11:21:35 +0800

    Remove duplicated operations

--- install_openstack.sh
+++ install_openstack.sh
@@ -2,10 +2,11 @@
 
 echo "Recommend to run the ./clear_openstack.sh first, especially for alreay installed machines."
 
+#This will add the rdo-release, puppetlabs and foreman yum source
 yum install -y http://rdo.fedorapeople.org/openstack/openstack-havana/rdo-release-havana.rpm  
-rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-yum install -y openstack-packstack nagios nagios-plugins-all nagios-plugins-nrpe nrpe puppet
+
+yum update -y
+yum install -y openstack-packstack
 
 #Avoid the failure occured by mirrorlist sometimes
 sed -i 's/^mirrorlist=https:\/\/mirrors.fedoraproject.org\/metalink?repo=epel-6&arch=$basearch/#mirrorlist=https:\/\/mirrors.fedoraproject.org\/metalink?repo=epel-6&arch=$basearch/g' /etc/yum.repos.d/epel.repo
@@ -13,6 +14,10 @@ sed -i 's/^mirrorlist=https:\/\/mirrors.fedoraproject.org\/metalink?repo=epel-6&
 service ntpd stop
 ssh 192.168.122.101 "service ntpd stop"
 ssh 192.168.122.101 "sed -i 's/^mirrorlist=https:\/\/mirrors.fedoraproject.org\/metalink?repo=epel-6&arch=$basearch/#mirrorlist=https:\/\/mirrors.fedoraproject.org\/metalink?repo=epel-6&arch=$basearch/g' /etc/yum.repos.d/epel.repo"
+
+
+#This will generate an answerfile template for allinone
+#packstack --gen-answer-file packstack-answers-template.txt
 
 #for vlan mode
 packstack --answer-file packstack-answers-vlan.txt
