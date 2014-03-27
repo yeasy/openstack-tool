1181a802fb3bd99c2faeeea30a6f0e95cc2b576c
Baohua Yang <baohyang@cn.ibm.com>
2014-03-27 14:27:21 +0800

    Check if br name is valid before dump flows

--- devop/ovs_mon.sh
+++ devop/ovs_mon.sh
@@ -4,39 +4,40 @@
 
 [ $# -lt 1 ] && echo "[Usage] $0 switch#1 switch#2 ..." && exit
 
-DUMP_CMD="sudo ovs-ofctl dump-flows"
+DUMP_FLOWS="sudo ovs-ofctl dump-flows"
+SHOW_BR="sudo ovs-vsctl show"
 priority=""
 rule=""
 result=""
-new_line=""
 tmp_file="/tmp/tmp_result.switch"
 
 [ -f  $tmp_file ] ||touch $tmp_file
 
 for arg in "$@"; do 
     echo "###"$arg
+    $SHOW_BR |grep -q $arg || echo "Non-Exist" && continue
     result=""
-    $DUMP_CMD $arg|sed -n '/actions=/p'|grep -v "n_packets=0" >$tmp_file
+    $DUMP_FLOWS $arg|sed -n '/actions=/p'|grep -v "n_packets=0" >$tmp_file
     while read line; do 
-        line=`echo $line|sed -e 's/idle_timeout=[0-9]\+,//'|sed -e 's/idle_age=[0-9]\+, //'|sed -e 's/hard_age=[0-9]\+, //'`
+        #line=`echo $line|sed -e 's/idle_timeout=[0-9]\+,//'|sed -e 's/idle_age=[0-9]\+, //'|sed -e 's/hard_age=[0-9]\+, //'`
+        nf=`echo $line|grep -o " "|wc -l`
         pkt=`echo $line|cut -d ' ' -f 4| sed -e 's/n_packets/PKT/'| sed -e 's/,//'`
-        priority_rule=`echo $line|cut -d ' ' -f 6| sed -e 's/_tci//'| sed -e 's/priority=//'`
-        if [ `expr match "$priority_rule" ".*,"` -ne 0 ]; then 
-            priority=`echo $priority_rule|cut -d ',' -f 1| sed -e 's/,//'` 
-            rule=`echo $priority_rule|cut -d ',' -f 2-| sed -e 's/\(..:\)\1\{1,\}/\1:/g'`
+        pri_rule=`echo $line|cut -d ' ' -f $nf|sed -e 's/_tci//'| sed -e 's/priority=//'`
+        if [ `expr match "$pri_rule" ".*,"` -ne 0 ]; then 
+            priority=`echo $pri_rule|cut -d ',' -f 1| sed -e 's/,//'` 
+            rule=`echo $pri_rule|cut -d ',' -f 2-|sed -e 's/\(..:\)\1\{1,\}/\1:/g'|sed -e 's/0x\(0\)\1\{1,\},/0x0,/g'`
         else
-            priority=$priority_rule
+            priority=$pri_rule
             rule="all"
         fi
-        action=`echo $line|cut -d ' ' -f 7| sed -e 's/actions/ACT/'`
-        new_line=$priority"\t"$pkt"\t"$rule"\t"$action"\n"
-        result=${result}${new_line}
+        action=`echo $line|cut -d ' ' -f $((nf+1))| sed -e 's/actions/ACT/'`
+        result=${result}$priority"\t"$pkt"\t"$rule"\t"$action"\n"
     done < $tmp_file
     echo -e $result|sort -n -r | while read line; do 
     priority=`echo $line |cut -d " " -f 1`
     pkt=`echo $line|cut -d " " -f 2`
     rule=`echo $line|cut -d " " -f 3`
     action=`echo $line|cut -d " " -f 4`
-    printf "%-6s %-12s %-60s %s\n" $priority $pkt $rule $action
+    printf "%-6s %-12s %-64s %s\n" $priority $pkt $rule $action
     done
 done
