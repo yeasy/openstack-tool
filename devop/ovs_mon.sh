8def17c723b434bb98a770e0dd3cb3cf4a547259
Baohua Yang <baohyang@cn.ibm.com>
2014-03-27 14:46:53 +0800

    Reformat the code

--- devop/ovs_mon.sh
+++ devop/ovs_mon.sh
@@ -6,12 +6,9 @@
 
 DUMP_FLOWS="sudo ovs-ofctl dump-flows"
 SHOW_BR="sudo ovs-vsctl show"
-priority=""
-rule=""
-result=""
 tmp_file="/tmp/tmp_result.switch"
 
-[ -f  $tmp_file ] ||touch $tmp_file
+[ -f  $tmp_file ] || touch $tmp_file
 
 for arg in "$@"; do 
     echo "###"$arg
@@ -19,25 +16,24 @@ for arg in "$@"; do
     result=""
     $DUMP_FLOWS $arg|sed -n '/actions=/p'|grep -v "n_packets=0" >$tmp_file
     while read line; do 
-        #line=`echo $line|sed -e 's/idle_timeout=[0-9]\+,//'|sed -e 's/idle_age=[0-9]\+, //'|sed -e 's/hard_age=[0-9]\+, //'`
         nf=`echo $line|grep -o " "|wc -l`
         pkt=`echo $line|cut -d ' ' -f 4| sed -e 's/n_packets/PKT/'| sed -e 's/,//'`
-        pri_rule=`echo $line|cut -d ' ' -f $nf|sed -e 's/_tci//'| sed -e 's/priority=//'`
-        if [ `expr match "$pri_rule" ".*,"` -ne 0 ]; then 
-            priority=`echo $pri_rule|cut -d ',' -f 1| sed -e 's/,//'` 
-            rule=`echo $pri_rule|cut -d ',' -f 2-|sed -e 's/\(..:\)\1\{1,\}/\1:/g'|sed -e 's/0x\(0\)\1\{1,\},/0x0,/g'`
+        pri_match=`echo $line|cut -d ' ' -f $nf|sed -e 's/_tci//'| sed -e 's/priority=//'`
+        if [ `expr match "$pri_match" ".*,"` -ne 0 ]; then 
+            priority=`echo $pri_match|cut -d ',' -f 1| sed -e 's/,//'` 
+            match=`echo $pri_match|cut -d ',' -f 2-|sed -e 's/\(..:\)\1\{1,\}/\1:/g'|sed -e 's/0x\(0\)\1\{1,\},/0x0,/g'`
         else
-            priority=$pri_rule
-            rule="all"
+            priority=$pri_match
+            match="all"
         fi
         action=`echo $line|cut -d ' ' -f $((nf+1))| sed -e 's/actions/ACT/'`
-        result=${result}$priority"\t"$pkt"\t"$rule"\t"$action"\n"
+        result=${result}$priority"\t"$pkt"\t"$match"\t"$action"\n"
     done < $tmp_file
     echo -e $result|sort -n -r | while read line; do 
-    priority=`echo $line |cut -d " " -f 1`
-    pkt=`echo $line|cut -d " " -f 2`
-    rule=`echo $line|cut -d " " -f 3`
-    action=`echo $line|cut -d " " -f 4`
-    printf "%-6s %-12s %-64s %s\n" $priority $pkt $rule $action
+        priority=`echo $line |cut -d " " -f 1`
+        pkt=`echo $line|cut -d " " -f 2`
+        match=`echo $line|cut -d " " -f 3`
+        action=`echo $line|cut -d " " -f 4`
+        printf "%-6s %-12s %-64s %s\n" $priority $pkt $rule $action
     done
 done
